0fd92f28ee33700bcbb1ba68b2740b2a3d661d90
Charles Miles <charles@cmiles.info>
2024-05-21 05:35:12 -0700

    TEP Backup Updated

---
+++ GmhWorkshop.TucsonElectricPowerBackup/TucsonElectricPowerBackupSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GmhWorkshop.CommonTools;
+
+namespace GmhWorkshop.TucsonElectricPowerBackup;
+public class TucsonElectricPowerBackupSettings : ISettingsFileType
+{
+    public const string SettingsTypeIdentifier = nameof(TucsonElectricPowerBackupSettings);
+    public string SettingsType { get; set; } = SettingsTypeIdentifier;
+    public string TepEmail { get; set; }
+    public string TepPassword { get; set; }
+    public string TepBackupDirectory { get; set; }
+}
