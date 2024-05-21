07d638d0e80fad1fc5fae6f8d4383c68b8d4828b
Charles Miles <charles@cmiles.info>
2024-05-21 05:14:39 -0700

    Tempest Weather Backup Updated

---
+++ GmhWorkshop.TempestWeatherBackup/TempestWeatherBackupSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GmhWorkshop.CommonTools;
+
+namespace GmhWorkshop.TempestWeatherBackup;
+public class TempestWeatherBackupSettings : ISettingsFileType
+{
+    public const string SettingsTypeIdentifier = nameof(TempestWeatherBackupSettings);
+    public string SettingsType { get; set; } = SettingsTypeIdentifier;
+    public string TempestAccessToken { get; set; }
+    public int TempestDeviceId { get; set; }
+    public string TempestFileBackupDirectory { get; set; }
+    public int TempestDaysBack { get; set; }
+}
